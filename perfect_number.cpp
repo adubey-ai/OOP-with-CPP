b57ab6ead70a994439c2fe9cb8b27fca6776f295
Adarsh Dubey <adarshiist@gmail.com>
2026-05-16 05:24:38 +0000

    Perfect Number in C++

---
+++ perfect_number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//main Program
+int main() {
+  int div, num, sum = 0;
+  cout << "Enter the number to check : ";
+  //user input
+  cin >> num;
+  //loop to find the sum of divisors
+  for (int i = 1; i < num; i++) {
+    div = num % i;
+    if (div == 0)
+      sum += i;
+  }
+  //checking for perfect number
+  if (sum == num)
+    cout << num << " is a perfect number.";
+  else
+    cout << num << " is not a perfect number.";
+  return 0;
+}
