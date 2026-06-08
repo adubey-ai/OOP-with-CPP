5498c020796c2439fe92992a3357595471d62d55
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-08 17:38:14 +0000

    Fix input handling in anagram C++ program

--- Anagram/anagram.cpp
+++ Anagram/anagram.cpp
@@ -1,44 +1,46 @@
-#include<iostream>
+#include <cctype>
+#include <iostream>
+#include <string>
 
 using namespace std;
 int main() {
-  //Initializing variables.
-  char str1[100], str2[100];
-  int first[26] = {
-    0
-  }, second[26] = {
-    0
-  }, c = 0, flag = 0;
+  // Initializing variables.
+  string str1, str2;
+  int first[26] = {0}, second[26] = {0};
+  int flag = 0;
 
-  //Accepting inputs.
+  // Accepting inputs.
   cout << "Enter First String: ";
-  gets(str1);
+  getline(cin, str1);
   cout << "Enter Second String: ";
-  gets(str2);
+  getline(cin, str2);
 
-  //Calculating frequencies of characters in first string.
-  while (str1[c] != '\0') {
-    first[str1[c] - 'a']++;
-    c++;
+  // Calculating frequencies of alphabetic characters in first string.
+  for (char ch : str1) {
+    if (isalpha(static_cast<unsigned char>(ch))) {
+      first[tolower(static_cast<unsigned char>(ch)) - 'a']++;
+    }
   }
 
-  c = 0;
-  //Calculating frequencies of characters in second string. 
-  while (str2[c] != '\0') {
-    second[str2[c] - 'a']++;
-    c++;
+  // Calculating frequencies of alphabetic characters in second string.
+  for (char ch : str2) {
+    if (isalpha(static_cast<unsigned char>(ch))) {
+      second[tolower(static_cast<unsigned char>(ch)) - 'a']++;
+    }
   }
-  //Checking if frequencies of both the strings are same or not.
-  for (c = 0; c < 26; c++) {
+
+  // Checking if frequencies of both strings are same or not.
+  for (int c = 0; c < 26; c++) {
     if (first[c] != second[c])
       flag = 1;
   }
-  //Priting result.
+
+  // Printing result.
   if (flag == 0) {
     cout << "Strings are anagram.";
   } else {
     cout << "Strings are not anagram.";
   }
-  return 0;
 
+  return 0;
 }
