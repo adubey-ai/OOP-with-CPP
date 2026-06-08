8f590e6bd7e049635400324fd79fb137696d3747
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-08 17:43:12 +0000

    Fix string palindrome program in C

--- String_Palindrome/string_palindrom.c
+++ String_Palindrome/string_palindrom.c
@@ -1,30 +1,30 @@
-#include 
-#include 
+#include <stdio.h>
+#include <string.h>
 
 // A function to check if a string str is palindrome
-voids isPalindrome(char str[])
+void isPalindrome(char str[])
 {
-// Start from leftmost and rightmost corners of str
-int l = 0;
-int h = strlen(str) - 1;
+	// Start from leftmost and rightmost corners of str
+	int l = 0;
+	int h = (int)strlen(str) - 1;
 
-// Keep comparing characters while they are same
-while (h > l)
-{
-if (str[l++] != str[h--])
-{
-printf("%s is Not Palindrome", str);
-return;
-}
-}
-printf("%s is palindrome", str);
+	// Keep comparing characters while they are same
+	while (h > l)
+	{
+		if (str[l++] != str[h--])
+		{
+			printf("%s is Not Palindrome\n", str);
+			return;
+		}
+	}
+	printf("%s is palindrome\n", str);
 }
 
 // Driver program to test above function
 int main()
 {
-isPalindrome("abba");
-isPalindrome("abbccbba");
-isPalindrome("geeks");
-return 0;
+	isPalindrome("abba");
+	isPalindrome("abbccbba");
+	isPalindrome("geeks");
+	return 0;
 }
