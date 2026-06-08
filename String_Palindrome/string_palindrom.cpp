c4bc5f3f4a6f061e480dcd677b9a12641d020811
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-08 17:38:15 +0000

    Repair broken string palindrome C++ implementation

--- String_Palindrome/string_palindrom.cpp
+++ String_Palindrome/string_palindrom.cpp
@@ -1,33 +1,22 @@
-#include 
-
-#include 
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
-
 {
+	string str1;
+	string str2;
 
-char str1[20], str2[20];
+	cout << "Enter the string : ";
+	getline(cin, str1);
 
-int i, j, len = 0, flag = 0;
+	str2 = string(str1.rbegin(), str1.rend());
 
-cout << "Enter the string : "; gets(str1); len = strlen(str1) - 1; for (i = len, j = 0; i >= 0 ; i--, j++)
+	if (str1 == str2)
+		cout << str1 << " is a palindrome";
+	else
+		cout << str1 << " is not a palindrome";
 
-str2[j] = str1[i];
-
-if (strcmp(str1, str2))
-
-flag = 1;
-
-if (flag == 1)
-
-cout << str1 << " is not a palindrome";
-
-else
-
-cout << str1 << " is a palindrome";
-
-return 0;
-
+	return 0;
 }
