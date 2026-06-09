365aa29c8941f82405ec6c72dcf7d4cd9af918e9
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-09 17:01:47 +0000

    vowel_or_consonant

--- vowel_or_consonant.c
+++ vowel_or_consonant.c
@@ -1,26 +1,28 @@
-#include 
+#include <stdio.h>
+
 int main()
 {
-char c;
-int isLowerVowel, isUpperVowel;
-printf("Enter an alphabet: ");
-scanf("%c",&c);
+	char c;
+	int isLowerVowel, isUpperVowel;
 
-//To find the corrector is lowercase vowel
-isLowerVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-//To find the character is Upper case vowel
-isUpperVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-// compare to charector is Lowercase Vowel or Upper case Vowel
+	printf("Enter an alphabet: ");
+	scanf(" %c", &c);
 
-if (isLowerVowel || isUpperVowel)
-printf("%c is a vowel", c);
-//to check character is alphabet or not
+	isLowerVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+	isUpperVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
-elseif((c >= 'a' && c= 'A' && c <= 'Z'))
-prinf("\n not a alphabet\n");
+	if (isLowerVowel || isUpperVowel)
+	{
+		printf("%c is a vowel", c);
+	}
+	else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		printf("%c is a consonant", c);
+	}
+	else
+	{
+		printf("\nnot an alphabet\n");
+	}
 
-else
-printf("%c is a consonant", c);
-
-return 0;
+	return 0;
 }
