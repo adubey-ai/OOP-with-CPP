cd5f016c91819c347645ed068a30e0d9fbeaa9cb
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-09 17:04:17 +0000

    vowel_or_consonant remove

--- vowel_or_consonant.py
+++
@@ -1,9 +0,0 @@
-#get user input
-Char =  input() 
-#Check if the Char belong to set of Vowels
-if (Char == 'a' or Char == 'e' or Char == 'i' or Char == 'o' or Char == 'u'): 
-    #if true 
-    print("Character is Vowel") 
-else: 
-    #if false
-    print("Character is Consonant")
