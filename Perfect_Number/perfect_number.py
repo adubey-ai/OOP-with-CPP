9515822c3718886c5d5d8523846bc12d59add1e0
adubey-ai <adubey-ai@users.noreply.github.com>
2026-06-09 15:50:34 +0000

    Fix perfect number script string syntax

--- Perfect_Number/perfect_number.py
+++ Perfect_Number/perfect_number.py
@@ -4,6 +4,6 @@ for i in range(1, n):
     if(n % i == 0):
         sump= sump + i
 if (sump == n):
-    print("The number is a Perfect number”)
+    print("The number is a Perfect number")
 else:
-    print("The number is not a Perfect number”)
+    print("The number is not a Perfect number")
